5f365555118c7dcf58e9bb446f991e02614613ae
Arinun <arinun.chainarong@gmail.com>
2022-11-10 23:02:23 +0700

    Add project files.

---
+++ Webbook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Webbook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
